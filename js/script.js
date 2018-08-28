44a9e339a6982b5f22a03a24d2b959ed3b509030
may54ther <aha9654@gmail.com>
2018-08-29 06:14:22 +0900

    ver3 commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    var wh = $(window).height();
+    var bool = true;
+
+    $('.sec_home').height(wh);
+
+    $(window).resize(function () {
+        wh = $(window).height();
+
+        $('.sec_home').height(wh);
+    });
+
+    $('.btn_scroll').click(function () {
+        $('html, body').stop().animate({
+            scrollTop: wh
+        }, 500);
+    });
+    
+    function offset(idx){
+        var offset = $("#sec" + idx).offset();
+        $('html, body').animate({scrollTop : offset.top}, 700);
+    }
+    
+
+    $('.btn_menu').click(function () {
+        if (bool == true) {
+            $('#gnb').slideDown("slow");
+            bool = false;
+        } else if (bool == false) {
+            $('#gnb').slideUp();
+            bool = true;
+        }
+
+        $('#gnb ul li a').click(function () {
+            var all = $('#gnb ul li').index();
+            var now = $(this).parents('li').index();
+
+            for (i = 0; i <= all; i++) {
+                $('#gnb').hide();
+                bool = true;
+                
+                if(now == i){
+                    offset(i+1);
+                }
+            }
+        });
+    });
+});
